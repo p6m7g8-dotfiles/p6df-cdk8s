d4d6bb5f7c1626e852bed81ae347f36bfd035128
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-10 15:55:10 -0400

    style: p6ify

--- init.zsh
+++ init.zsh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 ######################################################################
 #<
 #
@@ -7,7 +8,6 @@
 ######################################################################
 p6df::modules::cdk8s::deps() {
   ModuleDeps=(
-    #	cdk8s-team/cdk8s
     p6m7g8-dotfiles/p6df-kubernetes
   )
 }
@@ -22,6 +22,8 @@ p6df::modules::cdk8s::deps() {
 p6df::modules::cdk8s::langs() {
 
   p6_js_npm_global_install "cdk8s-cli"
+
+  p6_return_void
 }
 
 ######################################################################
@@ -29,23 +31,13 @@ p6df::modules::cdk8s::langs() {
 #
 # Function: p6df::modules::cdk8s::clones()
 #
-#  Depends:	 p6_run
-#  Environment:	 P6_DFZ_SRC_P6M7G8_DIR
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
 #>
 ######################################################################
 p6df::modules::cdk8s::clones() {
 
   p6_run_parallel "0" "4" "$(cat $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-cdk8s/conf/cdk8s)" "p6_github_util_repo_clone_or_pull_no_ou" "$P6_DFZ_SRC_FOCUSED_DIR"
+
+  p6_return_void
 }
 
-######################################################################
-#<
-#
-# Function: p6df::modules::cdk8s::init()
-#
-#>
-######################################################################
-p6df::modules::cdk8s::init() {
-
-  true
-}
